03910e60dcfff585c2fee834c2c38da46cb4ae48
Atul Varma <varmaa@gmail.com>
2010-08-02 22:02:17 -0400

    moved all commonJS modules into one file for simplicity; we can move them out into separate files again if/when it becomes cumbersome.

--- js/tests/test-all.js
+++
@@ -1,9 +0,0 @@
-require.def(
-  "tests/test-all",
-  ["top-hat"],
-  function(topHat) {
-    test("sample test",
-         function() {
-           ok(topHat.foo, "topHat.foo is truthy");
-         });
-  });
--- js/top-hat.js
+++ js/top-hat.js
@@ -1,6 +1,39 @@
 require.def(
-  "top-hat",
+  "need",
   [],
   function() {
-    return {foo: true};
+    function defaultDecay() {
+      return this.level - this.decayRate;
+    }
+
+    function defaultAttenuation() {
+      return "TODO";
+    }
+
+    function SimpleNeed(options) {
+      this.level = options.level || 0;
+      if ('decayRate' in options)
+        this.decayRate = options.decayRate;
+      this.decay = options.decay || defaultDecay;
+      this.attenuation = options.attenuation || defaultAttenuation;
+    };
+
+    SimpleNeed.prototype = {
+      decayRate: 5
+    };
+
+    return {
+      SimpleNeed: SimpleNeed
+    };
+  });
+
+require.def(
+  "tests/test-all",
+  ["need"],
+  function(need) {
+    test("SimpleNeed tests",
+         function() {
+           var sneed = new need.SimpleNeed({level: 0.1});
+           equals(sneed.level, 0.1, "options.level sets need.level");
+         });
   });
